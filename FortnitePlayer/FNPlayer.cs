40cd787a5b7994d3f61adb774a90a4fd50c15b2b
PmaiWoW <PmaiWoW@Gmail.com>
2019-10-02 16:47:36 +0100

    Exercício 3 da aula 1

---
+++ FortnitePlayer/FNPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FortnitePlayer
+{
+    public class FNPlayer
+    {
+        private string skin;
+        private int hp;
+
+        private void TakeDamage(int dmg)
+        {
+            hp -= dmg;
+            if (hp < 0) hp = 0;
+            // hp = Math.Max(0, hp - dmg)
+        }
+
+        private void Heal(int healAmount)
+        {
+            hp = Math.Min(100, hp + healAmount);
+        }
+
+        private void Shoot(FNPlayer enemy)
+        {
+            enemy.TakeDamage(10);
+        }
+    }
+}
